c860c3090714b6717b642d50182bb19b66985f27
naku.tenshi <naku.tenshii@gmail.com>
2026-01-14 20:00:25 +0330

    adding ./install.sh

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+if [[ "$EUID" -ne 0 ]]; then
+    echo "Please run as root (use sudo)"
+    exit 1
+fi
+
+file='./JWTKeyCracker.py'
+name="$(basename "${file%.py}")"
+
+cp -r $file /bin/$name
+echo "Done."
+
